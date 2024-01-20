fa6aaf4215bb1f8865c808e6d818afe8b0cafb1e
Ptalati015 <149040037+Ptalati015@users.noreply.github.com>
2024-01-20 16:48:00 -0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -60,7 +60,7 @@ function getResponse() {
     let userText = $("#textInput").val();
 
     if (userText == "") {
-        userText = "Liked!";
+        userText = "Liked!!";
     }
 
     let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
